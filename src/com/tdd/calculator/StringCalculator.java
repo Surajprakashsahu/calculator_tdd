e5c37f235fd0605127db6236906930d761eaf670
SurajS <suraj.sahu@draytus.com>
2022-05-17 23:37:25 +0530

    resolved for single number string test

--- src/com/tdd/calculator/StringCalculator.java
+++ src/com/tdd/calculator/StringCalculator.java
@@ -1,10 +1,15 @@
 package com.tdd.calculator;
 
+import java.util.List;
+
 public class StringCalculator {
 
     public int Add(String numbers){
         if(numbers.equals(""))
             return 0;
+        if(numbers.length() == 1){
+            return Integer.parseInt(numbers);
+        }
 
         return 0;
     }
