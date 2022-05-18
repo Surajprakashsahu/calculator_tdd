4ea33ebc91b61aa6e57e96503a3144c4cb45e8b4
Suraj Prakash Sahu <surajprakashsahu1@gmail.com>
2022-05-18 09:59:44 +0530

    check multiple negative values

--- src/com/tdd/calculator/StringCalculator.java
+++ src/com/tdd/calculator/StringCalculator.java
@@ -1,13 +1,15 @@
 package com.tdd.calculator;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StringCalculator {
+    final String INVALID_INP = "Invalid input.";
+    public int Add(String numbers) throws RuntimeException {
 
-    public int Add(String numbers) throws Exception {
         String delimiters = ",|\n";
-
 
         if(numbers.equals(""))
             return 0;
@@ -21,22 +23,38 @@ public class StringCalculator {
         }
 
         if(numbers.contains(",\n") || numbers.contains("\n,")){
-            throw new Exception("Invalid Input.");
+            throw new RuntimeException(INVALID_INP);
         }
 
-        List<String> numberList = Arrays.asList(numbers.split(delimiters));
+        List<Integer> numberList = Arrays.asList(numbers.split(delimiters)).stream().map((numstr -> {
+            try {
+                return parseToInt(numstr);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        })).collect(Collectors.toList());
         int sum = 0;
-        for(String num: numberList){
-            sum += parseToInt(num);
+        List<Integer> negatives = new ArrayList<>();
+        for(Integer num: numberList){
+            if(num < 0){
+                negatives.add(num);
+            }
+            sum += num;
+        }
+        if(negatives.size() > 0){
+            if(negatives.size() == 1)
+                throw new RuntimeException("negatives not allowed");
+
+            throw new RuntimeException(negatives.stream().map(a -> a+"").collect(Collectors.joining(",")));
         }
         return sum;
     }
 
-    public int parseToInt(String numStr)throws Exception{
+    public int parseToInt(String numStr)throws RuntimeException{
         try{
             return Integer.parseInt(numStr);
         }catch (Exception e){
-            throw new Exception("Invalid input.");
+            throw new RuntimeException(INVALID_INP);
         }
     }
 }
--- test/com/tdd/calculator/StringCalculatorTest.java
+++ test/com/tdd/calculator/StringCalculatorTest.java
@@ -1,11 +1,15 @@
 package com.tdd.calculator;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
 public class StringCalculatorTest {
 
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
     @Test
     public void testAdd() throws Exception {
         StringCalculator stringCalculator = new StringCalculator();
@@ -17,12 +21,31 @@ public class StringCalculatorTest {
         assertEquals(6, stringCalculator.Add("1\n2,3"));
         assertEquals(20, stringCalculator.Add("1\n2,3,4\n10"));
         assertEquals(3, stringCalculator.Add("//;\n1;2"));
+
     }
 
-    @Test(expected = Exception.class)
+    @Test()
     public void invalidInput() throws Exception {
         StringCalculator stringCalculator = new StringCalculator();
+        exceptionRule.expect(RuntimeException.class);
+        exceptionRule.expectMessage("Invalid input.");
         stringCalculator.Add("1,\n2");
         stringCalculator.Add("1,\n");
+    }
+
+    @Test()
+    public void negativeTest() throws Exception {
+        StringCalculator stringCalculator = new StringCalculator();
+        exceptionRule.expect(RuntimeException.class);
+        exceptionRule.expectMessage("negatives not allowed");
+        stringCalculator.Add("-1,2,3");
+    }
+
+    @Test()
+    public void multipleNegativeTest() throws Exception {
+        StringCalculator stringCalculator = new StringCalculator();
+        exceptionRule.expect(RuntimeException.class);
+        exceptionRule.expectMessage("-1,-3,-5");
+        stringCalculator.Add("-1,2,-3,4,-5");
     }
 }
